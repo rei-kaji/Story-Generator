13a38b20a0702b2e32d6131e72b7baaa41d4ec36
rei_kaji <st.r.z.rei@gmail.com>
2023-02-17 09:41:30 -0800

    update home

--- frontend/src/pages/storydetail/[storyId]/index.jsx
+++ frontend/src/pages/storydetail/[storyId]/index.jsx
@@ -61,23 +61,44 @@ const index = ({ post }) => {
       });
   };
 
-  const getComments = () => {
+  const getComments = async () => {
     let token = localStorage.getItem("token");
     // console.log("token", token);
     let data = { storyId: _id };
-    axios
-      .get(`${hostUrl}/api/comment/comments`, {
-        headers: {
-          uid: `${storyId}`,
-        },
-      })
-      .then((res) => {
-        console.log("res", res.data.comments);
-        setUserComments(res.data.comments);
-      })
-      .catch((err) => {
-        console.log("Something error happened at getComments", err);
-      });
+    // axios
+    //   .get(`${hostUrl}/api/comment/comments`, {
+    //     headers: {
+    //       "Access-Control-Expose-Headers": "uid",
+    //       uid: `${storyId}`,
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log("res", res.data.comments);
+    //     setUserComments(res.data.comments);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Something error happened at getComments", err);
+    //   });
+    try {
+      const response = await fetch(
+        // `/api/generate-story?word1=${word1}&word2=${word2}&word3=${word3}`
+        `${hostUrl}/api/comment/comments`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Expose-Headers": "uid",
+            uid: `${storyId}`,
+          },
+        }
+      );
+
+      const respppp = await response.json();
+      console.log("respppp", respppp);
+      // const { generatedStory } = await response.json();
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   useEffect(() => {
